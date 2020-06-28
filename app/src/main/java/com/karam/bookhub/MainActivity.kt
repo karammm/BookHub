b4cb7425fa9a212d5471b0ac1c1ea1b3150f9fde
karammm <karamjitverma9@gmail.com>
2020-06-28 23:55:24 +0530

    side drawer

--- app/src/main/java/com/karam/bookhub/MainActivity.kt
+++ app/src/main/java/com/karam/bookhub/MainActivity.kt
@@ -2,11 +2,49 @@ package com.karam.bookhub
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var drawerLayout: DrawerLayout
+    lateinit var coordinatorLayout: CoordinatorLayout
+    lateinit var toolbar:Toolbar
+    lateinit var frameLayout: FrameLayout
+    lateinit var navigationView: NavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        drawerLayout=findViewById(R.id.drawerLayout)
+        coordinatorLayout=findViewById(R.id.coordinatorLayout)
+        toolbar=findViewById(R.id.toolbar)
+        frameLayout=findViewById(R.id.frameLayout)
+        navigationView=findViewById(R.id.navigationView)
+        setUpToolBar()
+
+        val actionBarDrawerToggle=ActionBarDrawerToggle(
+            this@MainActivity ,
+            drawerLayout,
+            R.string.open_drawer,
+            R.string.close_drawer
+        )
+        drawerLayout.addDrawerListener(actionBarDrawerToggle)
+        //this will set a click listner on the hambugger icon whc is the action bar toggle
+        actionBarDrawerToggle.syncState()
+
     }
+    fun setUpToolBar(){
+        setSupportActionBar(toolbar)
+        supportActionBar?.title="Book Hub"//this ?mark is a null safety operator
+        //since the title bar can be null as sometime we dont have a toolbar on the screen
+        supportActionBar?.setHomeButtonEnabled(true)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
 }
